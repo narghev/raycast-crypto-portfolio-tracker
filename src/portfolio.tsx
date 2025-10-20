231f7e0250d6dc09d0c064d217bd673426d7d74a
narghev <narghev@protonmail.com>
2025-10-21 00:02:55 +0400

    wip ethereum portfolio list

--- src/portfolio.tsx
+++ src/portfolio.tsx
@@ -1,9 +1,29 @@
+import { List } from "@raycast/api";
 import { usePortfolio } from "./hooks/usePortfolio";
+import { supportedTokens, tokenDisplayProperties } from "./util/tokens";
 
 export default function Command() {
-  const { portfolio } = usePortfolio();
+  const { portfolio, isLoading } = usePortfolio();
 
-  console.log({ portfolio: JSON.stringify(portfolio) });
+  return (
+    <List isLoading={isLoading} searchBarPlaceholder="Search addresses..." isShowingDetail>
+      {supportedTokens.map((token) => {
+        const portfolioToken = portfolio[token];
+        const entries = Object.entries(portfolioToken);
+        const tokenDisplay = tokenDisplayProperties[token];
 
-  return null;
+        return (
+          <List.Section
+            key={token}
+            title={tokenDisplay.name}
+            subtitle={`${entries.length.toString()} address${entries.length !== 1 ? "es" : ""}`}
+          >
+            {entries.map(([address, addressInfo]) => (
+              <List.Item key={address} title={addressInfo.name} icon={{ source: tokenDisplay.image }} />
+            ))}
+          </List.Section>
+        );
+      })}
+    </List>
+  );
 }
